0df36817eaecd6bf93edbcf8c9724464ed20621a
JeckCowine <93724872+JeckCowine@users.noreply.github.com>
2021-11-09 11:51:58 +0100

    Update MainMailBoxAsinc.c

--- Produttore_Consumatore/4_MailBoxAsincrona/MainMailBoxAsinc.c
+++ Produttore_Consumatore/4_MailBoxAsincrona/MainMailBoxAsinc.c
@@ -8,22 +8,24 @@ int main(){
 	key_t Chiave_CODA = ftok ("./MailBoxAsinc",'M');	// chiave della coda messaggio
 
 		//Inizializzazione MailBox
-	
+
+	// assegnazione coda di comunicazione	
 	int ds_coda = msgget(Chiave_CODA,IPC_CREAT|0664);
 	if(ds_coda<0) { perror("MailBox errore"); exit(1); }
 	MailBox m; //Struct 
-
-	//FINE
-	
+	// inizializzazione code di servizio
 	Set_Dati(m);
 	
+	//FINE
+
+	// generazione produttore e consumatore	
 	for(int i=0;i<Num_Produttori;i++){
 			pid = fork();
 			if(pid==0) {
 				//figlio Produttore
 			int count=i;
 			sleep(2);
-			Produttore(m, ds_coda,count);
+			Produttore(m,ds_coda,count);
 			exit(1);
 			}
 	}
@@ -34,17 +36,17 @@ int main(){
 				//figlio Consumatore
 			int count=i;
 			sleep(2);
-			Consumatore(m, ds_coda,count);
+			Consumatore(m,ds_coda,count);
 			exit(1);
 			}
 		}
-
-
+	// attesa di terminazione
 	for(int i=0;i<Num_Consumatori+Num_Produttori; i++){
 		//fine produttori consumatori
 		wait(NULL);
 	}
     
+    // deallocazione code
 	Remove_Dati(m,ds_coda);
 	
 	return 0;
