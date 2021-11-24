3d959c3405f8f8f4dbbcdb6394c7d200d3c5b39c
JeckCowine <93724872+JeckCowine@users.noreply.github.com>
2021-11-24 12:27:23 +0100

    Update Processo2.c

--- 1_Esercizi/1_Esecizio_MailBox_3processi/Processo2.c
+++ 1_Esercizi/1_Esecizio_MailBox_3processi/Processo2.c
@@ -12,6 +12,7 @@ int main(){
 	Coda c;
 	
 	c.tipo=p2;
+	int check;
 	
 	for(int i=0;i<DIM_MSG_p2;i++){ //INVIA 5 MESSAGGI
 		
@@ -25,6 +26,7 @@ int main(){
 		c.stringa[DIM_STRING-1]='\0'; //CARATTERE TERMINALE
 		
 		msgsnd (ds_coda,(void*) &c,sizeof(Coda) - sizeof(long),IPC_NOWAIT); //INVIO MESSAGGIO SU CODA
+		if(check<0) {perror("msgsnd error");_exit(1);}
 		
 		//STAMPE VALORI INVIATI
 		printf("Mesaggio Inviato<n°%d>, PROCESSO < %lu > \n",i+1,c.tipo);
