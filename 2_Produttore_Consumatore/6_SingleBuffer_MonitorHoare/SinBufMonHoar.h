7a1d6a116c8d28683440be227dd35fa07f53d072
JeckCowine <93724872+JeckCowine@users.noreply.github.com>
2021-12-06 19:26:31 +0100

    Update SinBufMonHoar.h

--- 2_Produttore_Consumatore/6_SingleBuffer_MonitorHoare/SinBufMonHoar.h
+++ 2_Produttore_Consumatore/6_SingleBuffer_MonitorHoare/SinBufMonHoar.h
@@ -16,10 +16,19 @@
 #define Varcond_Produttori 0
 #define Varcond_Consumatori 1
 
+#define DIM_INT 3 // Dimensione Array
+#define DIM_STRING 10 // Dimensione Stringa
+
 typedef struct{
 	//VALORE DA PASSARE	
 	int intero; //PER PASSARE VALORE INT
-
+		    /*---oppure---*/
+	int array[DIM_INT];//PER PASSARE VALORE INT
+		    /*---oppure---*/
+	char carattere;//PER PASSARE VALORE CHAR
+		    /*---oppure---*/
+	char stringa[DIM_STRING];//PER PASSARE VALORE CHAR
+	
 	int buffer_libero;
 	int buffer_occupato;
 	
